766ad8a12eceb89cacbdd8e5f762aa19bb7d5c78
Yamila <yamilarobles10@gmail.com>
2024-06-17 04:15:40 +0200

    Add changes in SimpleCardComponent

--- pruebaTecnica/src/components/simpleCard/SimpleCard.jsx
+++ pruebaTecnica/src/components/simpleCard/SimpleCard.jsx
@@ -3,41 +3,48 @@ import "./simplecard.css";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
+import { CardMedia, Grid } from "@mui/material";
+import { CardHeader } from "react-bootstrap";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import drugImage from "./../../assets/img/drug.jpg";
 
 const SimpleCard = ({ applicationNumber, brandName, sponsorName }) => {
-  
   return (
-
-      <Card elevation={5}  sx={{ mt: 2 }}>
-        <CardContent>
-        <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-            ApplicationNumber:
-          </Typography>
-          <Typography component="div">
-            {applicationNumber}
-          </Typography>
-          <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-            Marca:
-          </Typography>
-          <Typography component="div">
-            {brandName}
-          </Typography>
-          <Typography sx={{ fontSize: 20 }} color="text.secondary" gutterBottom>
-            Patrocinador:
-          </Typography>
-          <Typography component="div">
-            {sponsorName}
-          </Typography>
-        </CardContent>
-        <CardActions>
-        <Link to={`/drug/${applicationNumber}`}>
-          <Button size="small">Ver más</Button>
-        </Link>
-        </CardActions>
-      </Card>
+    <Card elevation={5} sx={{ mt: 2, maxWidth: "80%", margin: "auto" }}>
+      <CardContent>
+        <Grid container spacing={2} direction="column">
+          <Grid item xs={12}>
+            <CardHeader>
+              <CardMedia
+                component="img"
+                height="194"
+                image={drugImage}
+                alt="Paella dish"
+              />
+            </CardHeader>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              variant="h6"
+              sx={{ fontSize: 15, fontWeight: "medium" }}
+              gutterBottom
+            >
+              {brandName}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body2" sx={{ fontSize: 12 }}>
+              {sponsorName}
+            </Typography>
+          </Grid>
+        </Grid>
+      </CardContent>    
+            <Link to={`/drug/${applicationNumber}`}>
+              <button display="flex"  justifyContent="center" className="custom-button"  >VER MÁS</button>
+            </Link>
+    </Card>
   );
 };
 
